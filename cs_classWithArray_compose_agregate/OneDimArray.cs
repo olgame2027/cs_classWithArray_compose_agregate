ba85bbfcfb6dc962840d90e38a691ab008f31da1
omm <olgame2027@gmail.com>
2023-03-01 13:30:55 +0300

    Add project files.

---
+++ cs_classWithArray_compose_agregate/OneDimArray.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace cs_classWithArray_compose_agregate
+{
+    internal class Vector
+    {
+        public string Title { get; set; }
+        public int[] Array { get; set; }
+
+        public Vector() { }
+
+        public Vector(string title, int[] array) {
+            Title = title;
+            Array = array;
+        }
+
+        public int SumPositive() {
+            int sum = 0;
+            foreach (int a in Array) { if (a > 0) sum += a; }
+            
+            return sum;
+        }
+
+        public int[] GetPosiveElements() {
+            int n = 0;
+            foreach (int a in Array) { if (a > 0) n++; }
+            int[] result = new int[n];
+            int i = 0;
+            foreach (int a in Array) {
+                if (a > 0) { result[i] = a; n++; }
+
+            }
+            return result;
+        }
+
+        public override bool Equals(object? obj) {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
+
+        public override string? ToString() {
+            return base.ToString();
+        }
+ 
+        
+    }
+}
---
+++ cs_classWithArray_compose_agregate/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace cs_classWithArray_compose_agregate
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+           
+            
+            //var x = a.GetValue(1); /// a[1]
+            //Console.WriteLine(x);
+            //int xx = (int)x + 5;
+            //a.SetValue(1000000, 0);
+
+            ////Console.WriteLine(b[0]);
+            ////Console.WriteLine(a[0]);
+            
+            int[] a = { 1, 2, 3 };
+            int[] aa = a;
+
+            int[] b = { 1, 2, 3 };
+
+            int[] c = new int[3];            
+            int[] d;
+
+       
+
+            a.CopyTo(c, 0);
+            Array.ForEach(c, Console.WriteLine);
+
+            d = (int[])a.Clone();
+            Array.ForEach(d, Console.WriteLine);
+
+
+        }
+    }
+}
